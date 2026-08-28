3604e2c5714ada1561ee3ebc348a32aa69e41bd4
NeKoRoSYS <nekorosys@gmail.com>
2026-08-28 19:49:19 +0800

    reduced redundancy

---
+++ gravensoft-website/src/data/routes/home.ts
@@ -0,0 +1,6 @@
+export const HOME = {
+    heroTitle: `Engineering experiences that leave a mark`,
+    heroSubtitle: `Here at GravenSoft, our team is commmitted to delivering you quality software solutions that scale.`,
+    testTitle: `Lorem Ipsum`,
+    testTest: `dolor sit amet, consectetur adipiscing elit. Quisque vel magna nec nulla sollicitudin consectetur. Donec non nisl ex. Sed sit amet libero sollicitudin, sodales orci sed, malesuada eros. Fusce nibh sapien, tincidunt quis dui a, luctus porta quam. Proin auctor sem non ante scelerisque luctus ac at diam. Integer erat quam, rutrum sed sapien molestie, iaculis scelerisque nisi. Nullam lobortis, neque sed ornare volutpat, nisi erat fringilla orci, ut mollis arcu ligula nec ex. Proin ex neque, vehicula ac molestie et, consectetur ac magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam sed lobortis metus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam facilisis urna eu purus viverra vestibulum. In eget tortor sed lectus finibus mattis. Nam faucibus tincidunt est at efficitur.`
+}
--- gravensoft-website/src/routes/Home.tsx
+++ gravensoft-website/src/routes/Home.tsx
@@ -1,24 +1,25 @@
 import Block from "../components/Block";
+import { HOME } from "../data/routes/home"
 
 export default function Home() {
   return (
     <>
       <Block classOverride={`bg-gray-900 min-h-[100svh]`}>
-        <h1 className="text-4xl font-bold">Engineering experiences that leave a mark</h1>
+        <h1 className="text-4xl font-bold">{HOME.heroTitle}</h1>
         <p>
-            Here at GravenSoft, our team is commmitted to delivering you quality software solutions that scale.
+            {HOME.heroSubtitle}
         </p>
       </Block>
       <Block classOverride={`bg-gray-800`}>
-        <h1 className="text-4xl font-bold">Lorem ipsum</h1>
+        <h1 className="text-4xl font-bold">{HOME.testTitle}</h1>
         <p>
-            dolor sit amet, consectetur adipiscing elit. Quisque vel magna nec nulla sollicitudin consectetur. Donec non nisl ex. Sed sit amet libero sollicitudin, sodales orci sed, malesuada eros. Fusce nibh sapien, tincidunt quis dui a, luctus porta quam. Proin auctor sem non ante scelerisque luctus ac at diam. Integer erat quam, rutrum sed sapien molestie, iaculis scelerisque nisi. Nullam lobortis, neque sed ornare volutpat, nisi erat fringilla orci, ut mollis arcu ligula nec ex. Proin ex neque, vehicula ac molestie et, consectetur ac magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam sed lobortis metus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam facilisis urna eu purus viverra vestibulum. In eget tortor sed lectus finibus mattis. Nam faucibus tincidunt est at efficitur.
+            {HOME.testTest}
         </p>
       </Block>
       <Block classOverride={`bg-gray-900`} borderVisible={false}>
-        <h1 className="text-4xl font-bold">Lorem ipsum</h1>
+        <h1 className="text-4xl font-bold">{HOME.testTitle}</h1>
         <p>
-            dolor sit amet, consectetur adipiscing elit. Quisque vel magna nec nulla sollicitudin consectetur. Donec non nisl ex. Sed sit amet libero sollicitudin, sodales orci sed, malesuada eros. Fusce nibh sapien, tincidunt quis dui a, luctus porta quam. Proin auctor sem non ante scelerisque luctus ac at diam. Integer erat quam, rutrum sed sapien molestie, iaculis scelerisque nisi. Nullam lobortis, neque sed ornare volutpat, nisi erat fringilla orci, ut mollis arcu ligula nec ex. Proin ex neque, vehicula ac molestie et, consectetur ac magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam sed lobortis metus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam facilisis urna eu purus viverra vestibulum. In eget tortor sed lectus finibus mattis. Nam faucibus tincidunt est at efficitur.
+            {HOME.testTest}
         </p>
       </Block>
     </>
