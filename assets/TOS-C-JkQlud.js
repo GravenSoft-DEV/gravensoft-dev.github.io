3f49ae92e6b2fa6e1f4c789f1e41a1aee8eb7bb9
NeKoRoSYS <nekorosys@gmail.com>
2026-09-01 14:14:00 +0000

    Deploying to live from @ GravenSoft-DEV/gravensoft-dev.github.io@7273d768e654cfdbd47c6a8f366963aef15c77ac 🚀

---
+++ assets/TOS-C-JkQlud.js
@@ -0,0 +1,1 @@
+import{o as e,s as t}from"./index-Cx41wXWG.js";import{t as n}from"./Block-Bx8Br3nx.js";var r=t();function i(){return e(`Terms of Service | GravenSoft`),(0,r.jsx)(`main`,{children:(0,r.jsx)(n,{className:`bg-zinc-950 min-h-svh`})})}export{i as default};
